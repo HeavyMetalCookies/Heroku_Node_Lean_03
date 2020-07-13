63b94d640c9603875c3fbf5333404bf4e5398712
JOHN_MARK_MADISON <HeavyMetalCookies@GMail.com>
2020-07-12 21:58:17 -0400

    [START_AT:README.HOW_TO_DEPLOY.TXT]

--- server.js
+++ server.js
@@ -12,7 +12,6 @@
     //: HN1_Ser_Fil      : Serve_File
     //: HN1_Mai          : MAIn_entry_point
 
-
 //:IMPORTS:
 
     const  D_U = process.env.DATABASE_URL ;          
@@ -47,7 +46,6 @@ const HN2_Get_fas =function( src_pat ){
     return( pro );
 };;
 
-
 const HN3_Run_cof
 =async function(
     rar
@@ -56,7 +54,7 @@ const HN3_Run_cof
     if( !rar    ){throw("[HN3_E06.A]"); };
     if( !rar[0] ){throw("[HN3_E06.B]"); };
     if( !rar[1] ){throw("[HN3_E06.C]"); };
-    if( !cof    ){throw("[HN3_E06.D]"); };
+    if( !cof    ){throw("[HN3_E05]"  ); };
 
     var cli=null;
     var err="[HN3_E01:NOT_SET]";
@@ -152,7 +150,10 @@ const HN3_Run_fas
 
         });;
      
-    };;
+    };; //:[hn3_executor]////////////////////////////////////://
+
+
+
     var pro=( new Promise( hn3_executor ) );
     return( pro );
 };;
@@ -179,7 +180,6 @@ const HN2_SQL_Get_Tes =function( rar_daw ){ "use strict"
         rar[1].end();
 
     });;
-
 };;
 
 const HN1_Ser_Fil =function( rar_daw ){ "use strict"
@@ -200,7 +200,6 @@ const HN1_Ser_Fil =function( rar_daw ){ "use strict"
 
         };;
     });;
-
 };;
 
 //://////////////////////////////////////////////////////////://
@@ -280,15 +279,6 @@ HN1_Mai();
 
 /**-*********************************************************-**
 
-           HN3_E01: HerokuNode(lean)03: Error #1
-           HN3_E02: HerokuNode(lean)03: Error #2
-           HN3_E03: HerokuNode(lean)03: Error #3
-           HN3_E04: HerokuNode(lean)03: Error #4
-           HN3_E05: HerokuNode(lean)03: Error #5
-           HN3_E06: HerokuNode(lean)03: Error #6
-           HN3_S01: HerokuNode(lean)03: Success #1
-               pgc: PostGres_Client, just use [ cli ]
-               cli: Client object. Probably PostGres PG client.
                  ": Double quote character. Same as single:'
                  ': Single quote character. Same as double:"
                  +: [ addition | string concat | english:And ]
@@ -307,6 +297,7 @@ HN1_Mai();
                 ;;: End a multi-line  statement
                 ==: Equality Comparison
                 []: Array literal or array access.
+                fs: file_system, built-in Node.js package.
                 if: Denotes condition required to execute block
                 to: Participle or denoting conversion.
                 {}: [ object / dictionary ] literal.
@@ -314,14 +305,17 @@ HN1_Mai();
                200: HTTP Status Code: Okay.
                ===: Strict Equality Comparison
                HN1: Heroku_Node_01 (Project Namespace)
+               POR: Shorthand. SEE[ PORT ]
                UTF: Unicode Transformation Format (ASCI++)
                act: Action to perform
+               cli: Client object. Probably PostGres PG client.
+               cof: ContentsOf_ile (file_path ==> file_contents)
                daw: DataAndWhatever
                end: Server is done talking to client.
                env: contains virtual machine's environment vars
-                fs: file_system, built-in Node.js package.
                new: New keyword instantiates instances
                pac: Path And Contenttype
+               pgc: PostGres_Client, just use [ cli ]
                pro: Promise instance
                rar: rar[0]==req, rar[1]==res
                req: REQuest  object
@@ -338,16 +332,15 @@ HN1_Mai();
               Html: A stupid way to write "HTML"
               PORT: PORT number server application listens on
               PORT: defined if deployed on Heroku or Azure
-               POR: Shorthand. SEE[ PORT ]
               else: Denotes alternative block of code.
               func: NOT a keyword. Function pointer variable.
               http: http package that comes with Node.js
+              http: hyper_text_transer_protocol
               then: Do this if promise accepted
              ROUTE: Relative url identifying page or resource
              alert: Display an alert box
              catch: Do this if promise rejected or error.
              const: immutable block-scope variable
-              http: hyper_text_transer_protocol
              serve: To deliver data from server to client
              sql._: A test SQL file to help figure things out.
              title: Node representing <title> element.
@@ -360,12 +353,19 @@ HN1_Mai();
            "utf-8": Unicode byte encoding. Extends: US-ASCII
            <title>: Denote title of html page
            DOCTYPE: Tell browser what markup language is used.
+           HN1_Mai: MAIn_entry_point
            HN1_Mai: Main entry point.
+           HN3_E01: HerokuNode(lean)03: Error #1
+           HN3_E02: HerokuNode(lean)03: Error #2
+           HN3_E03: HerokuNode(lean)03: Error #3
+           HN3_E04: HerokuNode(lean)03: Error #4
+           HN3_E05: HerokuNode(lean)03: Error #5
+           HN3_E06: HerokuNode(lean)03: Error #6
+           HN3_S01: HerokuNode(lean)03: Success #1
            Minimal: No extranious moving parts.
            Promise: Promise class built into NodeJS
            charset: Denote character encoding of file. 
            dat_fil: DEPRECATED_USE[ cof ](cof:ContentsOfFile)
-               cof: ContentsOf_ile (file_path ==> file_contents)
            example: Show you how it is done
            jum_dic: JUMp_DICtionary (Like a jumptable)
            not_nil: Denote object is not [nil/null]
@@ -388,7 +388,11 @@ HN1_Mai();
         Javascript: Poorly capitalized "JavaScript"
        "text/html": "Content-Type" for html files
        HN1_Ser_Fil: HN1_Serve_File: Serves a file to client.
+       HN1_Ser_Fil: Serve_File
        HN2_Get_fas: Get FileAsString(fas)
+       HN2_Get_fas: Get_file_as_string
+       HN3_Run_cof: Run_contents_of_file
+       HN3_Run_fas: Run_file_as_string
        Heroku_Node: Denotes a project using Heroku & Node.js
       "text/plain": "Content-Type" for plain text, NOT code.
       "use strict": Warnings are errors.
@@ -403,6 +407,7 @@ HN1_Mai();
    " Content-Type": Header indicating media type of resource.
    <!DOCTYPE HTML>: Tell browser document type is HTML.
    HN2_SQL_Get_Tes: Get SQL file as string test.
+   HN2_SQL_Get_Tes: SQL_Get_Test
   "DOCUMENT_TITLE": Placeholder string.
 "tex t/javascript": "Content-Type" for javascript files
 const A=(B)=>{...}: function "A" taking param "B"
